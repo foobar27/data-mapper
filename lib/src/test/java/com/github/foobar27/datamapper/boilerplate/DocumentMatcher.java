4c90a5a1b2ad409572689977004d927eb7acae03
Sébastien Wagener <sebastien.wagener@gmail.com>
2021-10-29 21:44:46 +0200

    Disable document matcher functionality (requires hamcrest)

--- lib/src/test/java/com/github/foobar27/datamapper/boilerplate/DocumentMatcher.java
+++ lib/src/test/java/com/github/foobar27/datamapper/boilerplate/DocumentMatcher.java
@@ -1,62 +1,62 @@
-package com.github.foobar27.datamapper.boilerplate;
-
-import com.github.foobar27.datamapper.testing.AbstractEntityMatcher;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
-
-import java.net.URL;
-import java.time.ZonedDateTime;
-
-import static com.github.foobar27.datamapper.boilerplate.DocumentSchema.documentSchema;
-
-public final class DocumentMatcher extends AbstractEntityMatcher {
-
-    // TODO create instances for DocumentPojo, ImmutableDocument etc
-
-    private DocumentMatcher() {
-        super(documentSchema());
-    }
-
-    public static DocumentMatcher document() {
-        return new DocumentMatcher();
-    }
-
-    // TODO concrete instances, for mutable, immutable etc
-
-    public DocumentMatcher url(URL value) {
-        return url(Matchers.is(value));
-    }
-
-    public DocumentMatcher url(Matcher<URL> matcher) {
-        registerMatcher(documentSchema().url(), matcher);
-        return this;
-    }
-
-    public DocumentMatcher publicationDate(ZonedDateTime value) {
-        return publicationDate(Matchers.is(value));
-    }
-
-    public DocumentMatcher publicationDate(Matcher<ZonedDateTime> matcher) {
-        registerMatcher(documentSchema().publicationDate(), matcher);
-        return this;
-    }
-
-    public DocumentMatcher title(String title) {
-        return title(Matchers.is(title));
-    }
-
-    public DocumentMatcher title(Matcher<String> matcher) {
-        registerMatcher(documentSchema().title(), matcher);
-        return this;
-    }
-
-    public DocumentMatcher content(String content) {
-        return content(Matchers.is(content));
-    }
-
-    public DocumentMatcher content(Matcher<String> matcher) {
-        registerMatcher(documentSchema().content(), matcher);
-        return this;
-    }
-
-}
+//package com.github.foobar27.datamapper.boilerplate;
+//
+//import com.github.foobar27.datamapper.testing.AbstractEntityMatcher;
+//import org.hamcrest.Matcher;
+//import org.hamcrest.Matchers;
+//
+//import java.net.URL;
+//import java.time.ZonedDateTime;
+//
+//import static com.github.foobar27.datamapper.boilerplate.DocumentSchema.documentSchema;
+//
+//public final class DocumentMatcher extends AbstractEntityMatcher {
+//
+//    // TODO create instances for DocumentPojo, ImmutableDocument etc
+//
+//    private DocumentMatcher() {
+//        super(documentSchema());
+//    }
+//
+//    public static DocumentMatcher document() {
+//        return new DocumentMatcher();
+//    }
+//
+//    // TODO concrete instances, for mutable, immutable etc
+//
+//    public DocumentMatcher url(URL value) {
+//        return url(Matchers.is(value));
+//    }
+//
+//    public DocumentMatcher url(Matcher<URL> matcher) {
+//        registerMatcher(documentSchema().url(), matcher);
+//        return this;
+//    }
+//
+//    public DocumentMatcher publicationDate(ZonedDateTime value) {
+//        return publicationDate(Matchers.is(value));
+//    }
+//
+//    public DocumentMatcher publicationDate(Matcher<ZonedDateTime> matcher) {
+//        registerMatcher(documentSchema().publicationDate(), matcher);
+//        return this;
+//    }
+//
+//    public DocumentMatcher title(String title) {
+//        return title(Matchers.is(title));
+//    }
+//
+//    public DocumentMatcher title(Matcher<String> matcher) {
+//        registerMatcher(documentSchema().title(), matcher);
+//        return this;
+//    }
+//
+//    public DocumentMatcher content(String content) {
+//        return content(Matchers.is(content));
+//    }
+//
+//    public DocumentMatcher content(Matcher<String> matcher) {
+//        registerMatcher(documentSchema().content(), matcher);
+//        return this;
+//    }
+//
+//}
